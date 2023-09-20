a1877ef02074161bd1beab596c8e52af6c8c4e99
Aoife Henry <aoife.henry@colorado.edu>
2023-09-20 10:19:58 -0600

    updates

--- nonlinear_analysis_job_script.sh
+++ nonlinear_analysis_job_script.sh
@@ -3,11 +3,11 @@
 #SBATCH --nodes=2
 #SBATCH --time=05:00:00
 #SBATCH --ntasks=96
-#SBATCH --partition=amem
+#SBATCH --partition=amilan
 #SBATCH --job-name=nonlin_sim
 #SBATCH --output=nonlin_sim.%j.out
 
 module purge
 module load matlab/R2022b
 cd /projects/aohe7145/projects/ipc_tuning/code
-matlab -nodisplay -nosplash -nodesktop -r "run('nonlinear_analysis.m');exit;" | tail -n +11
+matlab -nodisplay -nosplash -nodesktop -r "run('nonlinear_simulations.m'); exit;" | tail -n +11
